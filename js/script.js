87885fcd350606ec37f610bf3293605f20868c91
dmytropk <dmytro18wa@gmail.com>
2019-11-23 17:14:58 -0800

    play when clicked implemented

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
+// listing the playlist
 const songs = [
     "bensound-allthat.mp3",
     "bensound-downtown.mp3",
     "bensound-groovyhiphop.mp3",
     "bensound-hipjazz.mp3",
     "bensound-retrosoul.mp3"
-]
+];
 
 const createSongList = () => {
-    const list = document.createElement('ol')
+    const list = document.createElement('ol');
 
     for(let i = 0; i < songs.length; i++) {
-        const item = document.createElement('li')
-        item.appendChild(document.createTextNode(songs[i]))
+        const item = document.createElement('li');
+        item.appendChild(document.createTextNode(songs[i]));
 
-        list.appendChild(item)
+        list.appendChild(item);
     }
-    return list
+    return list;
 }
 
-document.getElementById('songList').appendChild(createSongList())
+document.getElementById('songList').appendChild(createSongList());
+
+// playing songs when clicked
+songList.onclick = (e) => {
+    // console.log(e);
+    const clickedItem = e.target;
+    const source = document.getElementById('source');
+    source.src = 'songs/' + clickedItem.innerText;
+    console.log(clickedItem.innerText);
+
+    player.load();
+    player.play();
+}
